45c132d500bdd530307628754196147b4888fa4e
Zaurtime <zaurteam@gmail.com>
2023-04-28 10:25:48 +0000

    Add views and templates for checkout

--- smart_cctv/settings.py
+++ smart_cctv/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
