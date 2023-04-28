53365705586e09e7d0ee91e73af6452324b09fae
Zaurtime <zaurteam@gmail.com>
2023-04-28 11:16:07 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N1onrA0Bi8wCwZZkbTjo5if6gRrYH5jkSrBLMcQGYFG6a32iyli6TEvaxdnA9pGkyIG2HuXuQFAMJMR1l1rC02o00WZr6Mem7',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
