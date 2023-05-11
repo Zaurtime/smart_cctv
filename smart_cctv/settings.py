7e7eb81cea7d4b0568c64ddb033ebbad9408f9a5
Zaurtime <zaurteam@gmail.com>
2023-05-11 10:55:15 +0000

    Add cloudinary to setting and requirements

--- smart_cctv/settings.py
+++ smart_cctv/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'contact',
     'newsletter',
     'testimonials',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -167,11 +168,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
