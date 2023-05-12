695b1bd78ae709ee8ff9d0a05058660a94a1d581
Zaurtime <zaurteam@gmail.com>
2023-05-12 14:39:50 +0000

    Add secret key and debug settings

--- smart_cctv/settings.py
+++ smart_cctv/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rr1b0(q^2pd&q24rrp9c()so=6*0vm+8(#x@m145=(2#@)ss_-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
