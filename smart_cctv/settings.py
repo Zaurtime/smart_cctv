f582a648e07d7183ebbb8c235788790ef2f777ac
Zaurtime <zaurteam@gmail.com>
2023-07-13 17:59:30 +0000

    Fix mail confirmation after purchase

--- smart_cctv/settings.py
+++ smart_cctv/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['smartcctv.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['smartcctv.herokuapp.com', '8000-zaurtime-smartcctv-2pyl4vg7lpx.ws-us101.gitpod.io']
 
 
 # Application definition
