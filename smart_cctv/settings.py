6a525a82a4d0ab94712655f8af2c2bfb72add95a
Zaurtime <zaurteam@gmail.com>
2023-07-17 03:51:31 +0000

    Deployment requirement change

--- smart_cctv/settings.py
+++ smart_cctv/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['smartcctv.herokuapp.com', '8000-zaurtime-smartcctv-2pyl4vg7lpx.ws-us101.gitpod.io']
 
