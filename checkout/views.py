1e48d857453785add01c2736e1c837a742dc5981
Zaurtime <zaurteam@gmail.com>
2023-07-20 18:14:11 +0000

    Add send email new function import

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpR
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
